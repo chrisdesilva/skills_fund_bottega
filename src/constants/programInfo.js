c76c47953a046f081de12cdaf308897c6a997710
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-25 09:56:11 -0500

    Fill in template with school info

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -1,4 +1,4 @@
-import logo from "../images/logo_placeholder.png" // add school logo to images folder and import here
+import logo from "../images/logo_bottega.jpeg" // add school logo to images folder and import here
 
 export const applicationsLive = true // set to false if not currently accepting applications
 export const disabledLoanAppFormID = '69140c47-bb03-4a1b-b674-cc136fdea23d' // Hubspot ID for all disabled apply now lead captures
@@ -9,74 +9,89 @@ export const nextCohortStartDate = 'October 29, 2019'
 export const schoolInfo = { // term details section
     interestRate36: '8.99%',
     interestRate60: '10.99%',
-    APRRange36: 'XX.XX - XX.XX%',
-    APRRange60: 'XX.XX - XX.XX%',
+    APRRange36: '10.87 - 11.69%',
+    APRRange60: '12.38 - 12.71%',
     interestOnly: [
         { // for multiple examples of interest only, add items to interestOnly array
-            programName: 'PROGRAM NAME',
-            APR36: 'XX.XX%',
-            financeCharge36: '$X,XXX.XX',
-            IOPayment36: '$XX.XX',
-            FullMonthlyPayment36: '$XXX.XX',
-            APR60: 'XX.XX%',
-            financeCharge60: '$X,XXX.XX',
-            IOPayment60: '$XX.XX',
-            FullMonthlyPayment60: '$XXX.XX',
+            programName: 'the Full-Time program',
+            APR36: '11.16%',
+            financeCharge36: '$2,293.64',
+            IOPayment36: '$77.91',
+            FullMonthlyPayment36: '$330.67',
+            APR60: '12.51%',
+            financeCharge60: '$4,040.39',
+            IOPayment60: '$95.25',
+            FullMonthlyPayment60: '$226.07',
             LoanExampleAmt: '$10,000',
             LoanExampleOFeeAmt: '$400',
             LoanExampleAmtPlusOFee: '$10,400',
-            programLength: 'X' // program length in months
-        }
+            programLength: '3' // program length in months
+        },
+        { // for multiple examples of interest only, add items to interestOnly array
+            programName: 'the Part-Time program',
+            APR36: '10.87%',
+            financeCharge36: '$1,953.97',
+            IOPayment36: '$58.44',
+            FullMonthlyPayment36: '$248.00',
+            APR60: '12.38%',
+            financeCharge60: '$3,316.04',
+            IOPayment60: '$71.44',
+            FullMonthlyPayment60: '$169.55',
+            LoanExampleAmt: '$7,500',
+            LoanExampleOFeeAmt: '$300',
+            LoanExampleAmtPlusOFee: '$7,800',
+            programLength: '7' // program length in months
+        },
     ],
     immediateRepayment: [
         { // for multiple examples of immmediate repayment, add items to immediateRepayment array
-            programName: 'PROGRAM NAME',
-            APR36: 'XX.XX%',
-            financeCharge36: '$X,XXX.XX',
-            FullMonthlyPayment36: '$XXX.XX',
-            APR60: 'XX.XX%',
-            financeCharge60: '$X,XXX.XX',
-            FullMonthlyPayment60: '$XXX.XX',
-            LoanExampleAmt: '$10,000',
-            LoanExampleOFeeAmt: '$400',
-            LoanExampleAmtPlusOFee: '$10,400'
+            programName: 'the Part-Time program',
+            APR36: '11.69%',
+            financeCharge36: '$1,428.06',
+            FullMonthlyPayment36: '$248.00',
+            APR60: '12.71%',
+            financeCharge60: '$2,673.12',
+            FullMonthlyPayment60: '$169.55',
+            LoanExampleAmt: '$7,500',
+            LoanExampleOFeeAmt: '$300',
+            LoanExampleAmtPlusOFee: '$7,800'
         }
     ]
 }
 
 export const faq = { // faq section
-    costOfLiving: false, // true if at least one program has cost of living included
-    costOfLivingPrograms: '', // leave as empty string is cost of living availability is the same across all programs
-    multCostOfLivingPrograms: true, // true if costOfLivingPrograms string has more than one program
+    costOfLiving: true, // true if at least one program has cost of living included
+    costOfLivingPrograms: 'the Full-Time program', // leave as empty string is cost of living availability is the same across all programs
+    multCostOfLivingPrograms: false, // true if costOfLivingPrograms string has more than one program
     interestOnly: true, // true if interest-only payments are an option
     immediateRepayment: true, // true if immediate repayment is an option
     multipleLoanLengths: true, // true if 36 and 60 month options are both available
     multipleLoanTypes: true, // true if both IR and IO are available
     multiPrograms: true, // only true if there are multiple programs
-    onlinePrograms: false, // true if at least one program is remote/online
-    schoolHQState: 'WA',
+    onlinePrograms: true, // true if at least one program is remote/online
+    schoolHQState: 'UT',
     origFee: 0.04,
 
     // interest payment FAQ info
     exampleLoanAmount: '$10,000',
     interestRate36: '8.99%',
     interestRate60: '10.99%',
-    APR36: 'XX.XX - XX.XX%',
-    APR60: 'XX.XX - XX.XX%',
+    APR36: '11.16%',
+    APR60: '12.51%',
     IOPayment36: '$77.91',
     IOPayment60: '$95.25',
 
     // max loan amounts by program for faq1
     loanRange: [
         {
-            programName: "PROGRAM 1",
-            maxAmount: "the max loan amount",
+            programName: "the Full-Time program",
+            maxAmount: "$12,000",
             col: true,
-            colAmount: "$6,000"
+            colAmount: "$4,000"
         },
         {
-            programName: "PROGRAM 2",
-            maxAmount: "$10,750",
+            programName: "the Part-Time program",
+            maxAmount: "$7,500",
             col: false,
             colAmount: "$6,000"
         },
@@ -89,43 +104,43 @@ export const faq = { // faq section
 
 export const schoolLogo = logo // go to header.js if height needs adjustment
 
-export const schoolName = "SCHOOL_NAME"
+export const schoolName = "Bottega"
 
-export const schoolURL = 'https://www.SCHOOLWEBSITE.com/' // update with url of school's website
+export const schoolURL = 'https://bottega.tech/' // update with url of school's website
 
-export const skfURL = 'https://SCHOOLNAME.skills.fund' // update with Skills Fund url
+export const skfURL = 'https://bottega.skills.fund/' // update with Skills Fund url
 
-export const headline = "Learn to Code at SCHOOL NAME" // update headline as appropriate
+export const headline = "Learn to Code at Bottega" // update headline as appropriate
 
 export const leadContent = {
     header: "Your last step on the path toward changing your career",
-    paragraph: `${schoolName} designs its programs to cover multiple stacks of programming languages to help graduates develop a diverse skill-set. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving ? ' and cost of living' : ''} financing so more students like you can access their program.`
+    paragraph: `${schoolName} offers full-time and part-time programs to provide students a foundation for a career in software development. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving ? ' and cost of living' : ''} financing so more students like you can access their program.`
 }
 
 export const threeStepCardText = {
     step1: "",
     step2: {
         header: "select your program",
-        text: "Choose between the Onsite Bootcamp, Online Full-Time Bootcamp, and Online Part-Time Bootcamp programs."
+        text: "Choose between the Full-Time On Campus, Full-Time Remote, and Part-Time Remote programs."
     },
     step3: `You'll be on your way to an exciting career in tech as part of ${schoolName}'s powerful network.`
 }
 
-export const netlifyFormName = 'SCHOOLNAME_contact'
+export const netlifyFormName = 'bottega_contact'
 
 export const GATracking = 'UA-68312423-1'
 
-export const hubspotFormId = "HUBSPOT_FORM_ID" // create Hubspot form, get form id after publishing
+export const hubspotFormId = "3f80933b-6751-4dca-8a9e-3b7c97814dfe" // create Hubspot form, get form id after publishing
 
-export const selectAProgram = "select_a_SCHOOLNAME_program" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
+export const selectAProgram = "select_a_bottega_program" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
 
 // ***** END GENERAL SCHOOL INFO ***** 
 
 
 // ***** BEGIN LOAN APP AND CALC INFO *****
 
-export const defaultLoanAmount = 10000
-export const placeholder = '$10,000'
+export const defaultLoanAmount = 12000
+export const placeholder = '$12,000'
 export const interestRates = {
     ir36: 8.99,
     ir60: 10.99
@@ -134,14 +149,14 @@ export const moreThanSixPrograms = false // set to true if there are 7 or more p
 
 export const programLoanInfo = [ // update with program names and corresponding loan URLs with market segment code from Master Loan Parameters
     {
-        name: "Program 1",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE1",
+        name: "Full-Time On Campus",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKBOTSD17",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 19495,
+            maxLoanAmt: 16000,
             loanTerm36: true,
             loanTerm60: true,
             '0': { // interest-only 
-                k: 5, 
+                k: 6, 
                 apr36: 11.16, 
                 apr60: 12.51
             },
@@ -155,11 +170,11 @@ export const programLoanInfo = [ // update with program names and corresponding 
           {
             location: "Metro 1",
             loanInfo: { // // match loanInfo to Program 1 above
-              maxLoanAmt: 19495,
+              maxLoanAmt: 16000,
               loanTerm36: true,
               loanTerm60: true,
               '0': { 
-                  k: 5, 
+                  k: 6, 
                   apr36: 11.16, 
                   apr60: 12.51
               },
@@ -197,14 +212,14 @@ export const programLoanInfo = [ // update with program names and corresponding 
         ]
     },
     {
-        name: "Program 2",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE2",
+        name: "Full-Time Remote",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKBOTSDR17",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 10995,
+            maxLoanAmt: 16000,
             loanTerm36: true,
             loanTerm60: true,
             '0': { 
-                k: 5, 
+                k: 6, 
                 apr36: 11.16, 
                 apr60: 12.51
             },
@@ -218,11 +233,11 @@ export const programLoanInfo = [ // update with program names and corresponding 
             {
               location: "Metro 1",
               loanInfo: { // match loanInfo to Program 2 loanInfo above
-                maxLoanAmt: 10995,
+                maxLoanAmt: 16000,
                 loanTerm36: true,
                 loanTerm60: true,
                 '0': { 
-                    k: 5, 
+                    k: 6, 
                     apr36: 11.16, 
                     apr60: 12.51
                 },
@@ -246,41 +261,41 @@ export const programLoanInfo = [ // update with program names and corresponding 
           ]
     },
     {
-        name: "Program 3",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE3",
+        name: "Part-Time Remote",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKBOTSDP17",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 15995,
+            maxLoanAmt: 7500,
             loanTerm36: true,
             loanTerm60: true,
             '0': { 
-                k: 5, 
-                apr36: 11.16, 
-                apr60: 12.51
+                k: 9, 
+                apr36: 10.87, 
+                apr60: 12.38
             },
             '1': {
-              apr36: 11.25, 
-              apr60: 12.55
+              apr36: 11.69, 
+              apr60: 12.71
           }
         },
         defaultLoanType: "0", // leave at 0 for interest-only, set to 1 for immediate repayment
         showMetros: false,
-        showLoanTypes: false,
+        showLoanTypes: true,
         locations: ["Metro A", "Metro B", "Metro C"],
         metros: [
             {
               location: "Metro A",
               loanInfo: { // match loanInfo to Program 3 loanInfo above
-                maxLoanAmt: 15995,
+                maxLoanAmt: 7500,
                 loanTerm36: true,
                 loanTerm60: true,
                 '0': { 
-                    k: 5, 
-                    apr36: 11.16, 
-                    apr60: 12.51
+                    k: 9, 
+                    apr36: 10.87, 
+                    apr60: 12.38
                 },
                 '1': {
-                    apr36: 11.25, 
-                    apr60: 12.55
+                    apr36: 11.69, 
+                    apr60: 12.71
                 }
               }
             },
@@ -323,7 +338,7 @@ export const programLoanInfo = [ // update with program names and corresponding 
 ]
 
 // ***** BEGIN LOAN CALC TEXT INFO *****
-export const programMaxText = "Choose the loan amount that works best for you. Borrow up to your metro's max (see table below) for the PROGRAM 1, up to $14,995 for the PROGRAM 2 Bootcamp tuition, and up to $9,995 for the PROGRAM 3 Bootcamp tuition."
+export const programMaxText = "Choose the loan amount that works best for you. Borrow up to $12,000 for tuition and up to $4,000 for cost of living for the full-time programs and up to $7,500 for tuition for the part-time program."
 
 export const paymentTable = {
     headers: ["Program", "Tuition", "Cost of Living", "Max Total"],
